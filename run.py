e7a3ecca4327a34d53278ba8d05aea17267fd1f2
Snow WhiteX <80204080+SnowWhiteX@users.noreply.github.com>
2022-02-18 14:26:51 +0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,51 @@
+import os
+import requests
+import socket
+import time
+import animator
+app = animator.Animator()
+app.scene("Starting dogesploit framework")
+app.scene("sTaRting dogesPloİt fRAmeWork")
+app.scene("starTİng dOgeSplOit FrAmewOrk")
+app.scene("STarting dogesploit FRAmeWorK")
+app.play()
+time.sleep(5)
+print("""
+ ____   ___   ____ _____ ____  ____  _     ___ _   _ __ _____
+|  _ \ / _ \ / ___| ____/ ___||  _ \| |   / _ (_)_(_)  \_   _|
+| | | | | | | |  _|  _| \___ \| |_) | |  | | | |/_\| () || |
+| |_| | |_| | |_| | |___ ___) |  __/| |__| |_| / _ \\__/ | |
+|____/ \___/ \____|_____|____/|_|   |_____\___/_/ \_\    |_|
+Exploits : 2  auxiliary : 2
+
+
+"""
+)
+
+time.sleep(5)
+while True:
+ cn = input("dg1> ")
+ if cn == "help":
+  print("list")
+  print("set")
+ if cn == "list":
+  print("exploit/web/http/admin_panel_search")
+  print("exploit/shh/command_send_recv")
+  print("auxiliary/web/inject")
+  print("auxiliary/web/web_cloner")
+ if cn == "set":
+  s = input("enter exploit name : ")
+  if s == "exploit/web/http/admin_panel_search":
+   os.system("python kod.py")
+  if s == "exploit/shh/command_send_recv":
+   os.system("python kod2.py")
+  if s == "auxiliary/web/inject":
+   os.system("python kod3.py")
+  if s == "auxiliary/web/web_cloner":
+   os.system("python kod4.py")
+   
+
+
+
+     
+   
