d61412e1201d67e0aebc0ae5d56cf2db6ec1fa19
TheSyrox <80204080+TheSyrox@users.noreply.github.com>
2022-05-07 21:26:40 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 rm -R *
-wget https://github.com/SnowWhiteX/DogesploitFramework/blob/main/animator.py
-wget https://github.com/SnowWhiteX/DogesploitFramework/blob/main/kod.py
-wget https://github.com/SnowWhiteX/DogesploitFramework/blob/main/kod2.py
-wget https://github.com/SnowWhiteX/DogesploitFramework/blob/main/kod3.py
-wget https://github.com/SnowWhiteX/DogesploitFramework/blob/main/kod4.py
-wget https://github.com/SnowWhiteX/DogesploitFramework/blob/main/kod5.py
-wget https://github.com/SnowWhiteX/DogesploitFramework/blob/main/run.py
+curl -O https://github.com/SnowWhiteX/DogesploitFramework/blob/main/animator.py
+curl -O https://github.com/SnowWhiteX/DogesploitFramework/blob/main/kod.py
+curl -O https://github.com/SnowWhiteX/DogesploitFramework/blob/main/kod2.py
+curl -O https://github.com/SnowWhiteX/DogesploitFramework/blob/main/kod3.py
+curl -O https://github.com/SnowWhiteX/DogesploitFramework/blob/main/kod4.py
+curl -O https://github.com/SnowWhiteX/DogesploitFramework/blob/main/kod5.py
+curl -O https://github.com/SnowWhiteX/DogesploitFramework/blob/main/run.py
 echo updated
