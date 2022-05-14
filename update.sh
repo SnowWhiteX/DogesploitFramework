6e236113ac1a472f108068718d93c0efd7b3754d
TheSyrox <80204080+TheSyrox@users.noreply.github.com>
2022-05-14 10:36:06 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,8 @@
 rm -R *
-curl -O https://github.com/SnowWhiteX/DogesploitFramework/blob/main/animator.py
-curl -O https://github.com/SnowWhiteX/DogesploitFramework/blob/main/kod.py
-curl -O https://github.com/SnowWhiteX/DogesploitFramework/blob/main/kod2.py
-curl -O https://github.com/SnowWhiteX/DogesploitFramework/blob/main/kod3.py
-curl -O https://github.com/SnowWhiteX/DogesploitFramework/blob/main/kod4.py
-curl -O https://github.com/SnowWhiteX/DogesploitFramework/blob/main/kod5.py
-curl -O https://github.com/SnowWhiteX/DogesploitFramework/blob/main/run.py
-echo updated
+curl --output animatör.py https://github.com/TheSyrox/DogesploitFramework/blob/main/animator.py
+curl --output kod.py https://github.com/TheSyrox/DogesploitFramework/blob/main/kod.py
+curl --output kod2.py https://github.com/TheSyrox/DogesploitFramework/blob/main/kod2.py
+curl --output kod3.py https://github.com/TheSyrox/DogesploitFramework/blob/main/kod3.py
+curl --output kod4.py https://github.com/TheSyrox/DogesploitFramework/blob/main/kod4.py
+curl --output kod5.py https://github.com/TheSyrox/DogesploitFramework/blob/main/kod5.py
+curl --output run.py https://github.com/TheSyrox/DogesploitFramework/blob/main/run.py
